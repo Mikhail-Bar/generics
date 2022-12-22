44eb48d3b19f2648df6e6f8f58c2ace74f92b58f
Mikhail <mikhail.barashkou@yandex.ru>
2022-12-22 19:21:39 +0300

    Initial commit

---
+++ src/com/company/Interview.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Interview {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <1;i++, System.out.println("Hello world")) {
+
+        }
+        if(System.out.printf("Hello world") == null){}
+        int i=0;
+        while (i<1 && System.out.printf("Hello world") == null){i++;}
+    }
+}
---
+++ src/com/company/Interview2.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Interview2 {
+    static {
+        System.out.println("Hello world");
+    }
+}
